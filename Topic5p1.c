0f30857a59ba3a26a5953b14d87694cba04493f1
Joshika5 <joshikadhanam95@gmail.com>
2026-02-11 23:09:36 +0530

    Add files via upload

---
+++ Topic5p1.c
@@ -0,0 +1,11 @@
+// 1.print natural number
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++) {
+        printf(" %d",i);
+    }
+    return 0;
+
+}
---
+++ Topic5p9.c
@@ -0,0 +1,12 @@
+// 9.Sum of even numbers
+#include<stdio.h>
+int main() {
+    int n;
+    int sum=0;
+    scanf("%d",&n);
+    for (int i=2;i<=n;i+=2) {
+        sum+=i;
+    }
+    printf("%d",sum);
+    return 0;
+}
